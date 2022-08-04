d1b7552f6545f69953a8f289c0b2dd75573f90e3
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2022-08-04 01:50:14 +0000

    Improve module

--- papi/__main__.py
+++ papi/__main__.py
@@ -3,16 +3,18 @@ import sys
 import pandas as pd
 
 DF_PATH = sys.argv[1]
-INPUT_USED_COLS = ['Order Date', 'Title', 'ASIN/ISBN', 'Condition', 'Quantity', 'Item Total']
+INPUT_USED_COLS = ['Order Date', 'Order ID', 'Title', 'ASIN/ISBN', 'Condition', 'Quantity', 'Item Total']
 
 df = pd.read_csv(DF_PATH, usecols=INPUT_USED_COLS, parse_dates=['Order Date'],
                 converters={'Item Total': lambda s: float(s.lstrip('$'))},
                 )
-df.rename(columns={'Order Date': 'Date', 'ASIN/ISBN': 'ASIN', 'Item Total': 'TotalCost'}, inplace=True)
+df.rename(columns={'Order Date': 'Date', 'Order ID': 'Order', 'ASIN/ISBN': 'ASIN', 'Item Total': 'TotalCost'}, inplace=True)
 df = df[df['Condition']=='new']
 del df['Condition']
 df = df[(df['Quantity'] >= 1) & (df['TotalCost'] > 0)]
 assert df['Date'].is_monotonic_increasing
 assert (df['ASIN'].notna() & (df['ASIN'] != "")).all()
+df.drop_duplicates(subset=['Order', 'ASIN'], inplace=True)
+del df['Order']
 print(df)
 print(df.dtypes)
