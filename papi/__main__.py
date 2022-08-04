830fdfdb9d50d33598e644189f583d0602ebdf33
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2022-08-04 01:06:04 +0000

    Improve main module

--- papi/__main__.py
+++ papi/__main__.py
@@ -3,6 +3,13 @@ import sys
 import pandas as pd
 
 DF_PATH = sys.argv[1]
-df = pd.read_csv(DF_PATH)
+INPUT_USED_COLS = ['Order Date', 'Title', 'ASIN/ISBN', 'Condition', 'Quantity', 'Item Total']
+
+df = pd.read_csv(DF_PATH, usecols=INPUT_USED_COLS, parse_dates=['Order Date'])
+df.rename(columns={'Order Date': 'Date', 'ASIN/ISBN': 'ASIN', 'Item Total': 'TotalCost'}, inplace=True)
+df = df[df['Condition']=='new']
+del df['Condition']
+assert df['ASIN'].notna().all()
+assert not (df['ASIN'] == "").any()
 print(df)
 print(df.dtypes)
