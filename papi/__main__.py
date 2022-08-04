1d26cf79f7db4b8bdff6489aabe9b875ea3b8e9b
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2022-08-04 04:35:24 +0000

    Improve module

--- papi/__main__.py
+++ papi/__main__.py
@@ -2,10 +2,11 @@ import sys
 
 import pandas as pd
 
-DF_PATH = sys.argv[1]
+SAMPLE_PATH = './sample.csv'
 INPUT_USED_COLS = ['Order Date', 'Order ID', 'Title', 'ASIN/ISBN', 'Condition', 'Quantity', 'Item Total']
 
-df = pd.read_csv(DF_PATH, usecols=INPUT_USED_COLS, parse_dates=['Order Date'],
+df_path = sys.argv[1] if sys.argv[1:] else SAMPLE_PATH
+df = pd.read_csv(df_path, usecols=INPUT_USED_COLS, parse_dates=['Order Date'],
                 converters={'Item Total': lambda s: float(s.lstrip('$').replace(',', ''))},
                 )
 df.rename(columns={'Order Date': 'Date', 'Order ID': 'Order', 'ASIN/ISBN': 'ASIN', 'Item Total': 'TotalCost'}, inplace=True)
@@ -24,4 +25,12 @@ df_date_ranges = df['Date'].drop_duplicates()[::-1].reset_index(drop=True).to_fr
 df_date_ranges['LaterBegin'] = df_date_ranges['LaterEnd'] - pd.DateOffset(years=1, days=-1)
 df_date_ranges['BaseEnd'] = df_date_ranges['LaterBegin'] - pd.DateOffset(days=1)
 df_date_ranges['BaseBegin'] = df_date_ranges['BaseEnd'] - pd.DateOffset(years=1, days=-1)
-print(df_date_ranges)
+oldest_order_date = df['Date'].min()
+df_date_ranges = df_date_ranges[df_date_ranges['BaseBegin'] >= oldest_order_date]
+# print(df_date_ranges)
+
+for date_range in df_date_ranges.itertuples():
+    df_later = df[(date_range.LaterBegin <= df['Date']) & (df['Date'] <= date_range.LaterEnd)]
+    df_base = df[(date_range.BaseBegin <= df['Date']) & (df['Date'] <= date_range.BaseEnd)]
+    print(date_range)
+    pass
