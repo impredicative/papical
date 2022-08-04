df27f0c36c57160d36f205eb779abdfd9df5559c
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2022-08-04 06:06:53 +0000

    Improve module

--- papi/__main__.py
+++ papi/__main__.py
@@ -9,10 +9,10 @@ df_path = sys.argv[1] if sys.argv[1:] else SAMPLE_PATH
 df = pd.read_csv(df_path, usecols=INPUT_USED_COLS, parse_dates=['Order Date'],
                 converters={'Item Total': lambda s: float(s.lstrip('$').replace(',', ''))},
                 )
-df.rename(columns={'Order Date': 'Date', 'Order ID': 'Order', 'ASIN/ISBN': 'Product', 'Item Total': 'Total'}, inplace=True)
+df.rename(columns={'Order Date': 'Date', 'Order ID': 'Order', 'ASIN/ISBN': 'Product', 'Item Total': 'Cost'}, inplace=True)
 df = df[df['Condition']=='new']
 del df['Condition']
-df = df[(df['Quantity'] >= 1) & (df['Total'] > 0)]
+df = df[(df['Quantity'] >= 1) & (df['Cost'] > 0)]
 assert df['Date'].is_monotonic_increasing
 assert (df['Product'].notna() & (df['Product'] != "")).all()
 df.drop_duplicates(subset=['Order', 'Product'], inplace=True)
@@ -29,12 +29,17 @@ oldest_order_date = df['Date'].min()
 df_date_ranges = df_date_ranges[df_date_ranges['BaseBegin'] >= oldest_order_date]
 # print(df_date_ranges)
 
+subset_columns = ['Product', 'Quantity', 'Cost']
 for date_range in df_date_ranges.itertuples():
-    df_later = df[(date_range.LaterBegin <= df['Date']) & (df['Date'] <= date_range.LaterEnd)]
-    df_base = df[(date_range.BaseBegin <= df['Date']) & (df['Date'] <= date_range.BaseEnd)]
+    df_later = df[(date_range.LaterBegin <= df['Date']) & (df['Date'] <= date_range.LaterEnd)][subset_columns]
+    df_base = df[(date_range.BaseBegin <= df['Date']) & (df['Date'] <= date_range.BaseEnd)][subset_columns]
     common_products = pd.merge(df_later['Product'], df_base['Product'], how='inner')['Product'].drop_duplicates()
-    if len(common_products) == 0:
+    if common_products.empty:
         continue
-    df_later = df_later[df_later['Product'].isin(common_products)]
-    df_base = df_base[df_base['Product'].isin(common_products)]
-    
+    df_later = df_later[df_later['Product'].isin(common_products)].groupby('Product').sum()
+    df_base = df_base[df_base['Product'].isin(common_products)].groupby('Product').sum()
+    df_later['AvgCost'] = df_later['Cost'] / df_later['Quantity']
+    df_base['AvgCost'] = df_base['Cost'] / df_base['Quantity']
+    del df_later['Cost']
+    del df_base['Cost']
+    pass
